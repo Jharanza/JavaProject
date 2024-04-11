efdc25c42153d176be2b5d69fdcb9c54b83284c5
Jhony Arana <jharanza1986@gmail.com>
2024-04-11 12:30:26 -0500

    feat: Use a variable

---
+++ Greeting.java
@@ -0,0 +1,8 @@
+public class Greeting {
+    public static void main(String[] args) {
+
+        String name = "Jhony"; // variable initialized of type String 
+
+        System.out.println("Hello " + name);
+    }
+}
